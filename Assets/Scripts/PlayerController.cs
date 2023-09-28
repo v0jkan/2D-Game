dc436bf73e678ef3bbfea62724414ca44542a559
v0jkan <vojkan02miletic@gmail.com>
2023-09-28 17:40:56 +0200

    Player Movement and Level Completion
    Created player script which includes player movement.
    Created finish line and fake finish line.
    Finish line displays message, while fake doesn't.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2d;
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rigidbody2d.velocity = new Vector2(speed, 0f);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rigidbody2d.velocity = new Vector2(-speed, 0f);
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            rigidbody2d.velocity = new Vector2(0f, speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            rigidbody2d.velocity = new Vector2(0f, -speed);
+        }
+        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            rigidbody2d.velocity = new Vector2(0f, 0f);
+        }  
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Finish")
+        {
+            Debug.Log("Level Completed!");
+        }
+    }
+}
